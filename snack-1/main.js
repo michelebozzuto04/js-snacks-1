64038ac38219c4ad3a181a5d42251bdd8062d058
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-05-30 10:48:01 +0200

    Implement number comparison functionality with user prompts

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,1 +1,11 @@
-console.log("prova")
+const num1 = prompt("Inserisci un numero:");
+const num2 = prompt("Inserisci un altro numero:");
+
+if (num1 > num2) {
+    console.log("Il numero più grande è:", num1);
+} else if (num2 > num1) {
+    console.log("Il numero più grande è:", num2);
+} else {
+    console.log("I numeri sono uguali");
+
+}
