e5c893a89f418f8a32d234a9aef5a1346847e1d2
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-05-30 11:30:17 +0200

    Fix output order for string length comparison in Snack 2

--- snack-2/main.js
+++ snack-2/main.js
@@ -2,9 +2,9 @@ const string1 = prompt("Inserisci una parola:");
 const string2 = prompt("Inserisci un'altroa parola:");
 
 if (string1.length > string2.length) {
-    console.log(string1, string2);
-} else if (string2.length > string1.length) {
     console.log(string2, string1);
+} else if (string2.length > string1.length) {
+    console.log(string1, string2);
 } else {
     console.log("Le due parole hanno la stessa lunghezza");
 }
